67195ba42d5e88a94ac3134c9d713fb52a71180c
Rajkawale7 <Rajkawale712@gmail.com>
2022-04-14 20:15:06 +0530

    Added: First case to check and validate the User's First Name

---
+++ src/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,33 @@
+package com.bridgelabz;
+import java.util.regex.*;
+import java.util.Scanner;
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		//As a User need to a valid First Name
+		Scanner scan = new Scanner (System.in);
+		int ch = 1;
+		do {
+		System.out.println("Enter Your First Name: ");
+		String f_name = scan.next();
+		
+		String regex = "^{1}[A-Z]+[a-z]";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(f_name);
+		
+		if (m.find()) {
+			System.out.println("Match found");
+		}
+		else {
+			System.out.println("Match not found");
+		}
+		
+		//To continue the same process by using do while loop
+		System.out.println("Continue...press 1,");
+		ch = scan.nextInt();
+		} 
+		while(ch==1);
+		
+	}
+
+}
