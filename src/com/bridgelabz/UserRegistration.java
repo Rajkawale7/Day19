fdd4965358e8ef88036971b28abd3a221a523a32
Rajkawale7 <Rajkawale712@gmail.com>
2022-04-15 13:00:05 +0530

    Added: Providing some email samples for validation

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -4,32 +4,26 @@ import java.util.Scanner;
 public class UserRegistration {
 
 	public static void main(String[] args) {
-		//As a User need to a follow predefined Unique Password format.
-		Scanner scan = new Scanner (System.in);
-		int ch = 1;
-		do {
-		System.out.println("Enter Your Unique Password: ");
-		String password = scan.next();
+		//User case UC-9
+		//Regex to check some valid Email Samples
+		String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
 		
-		//Compiling the regex
-		String regex = "^(?=.*[0-9])"
-				+ "(?=.*[a-z])(?=.*[A-Z])"
-                + "(?=.*[@#$%^&+=])"
-                + "(?=\\S+$).{8,20}$";
-		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(password);	
+		String Email1 = "Rajkawal00@gmail.com";		
+		System.out.println(Pattern.compile(regex).matcher(Email1).matches());
 		
-		if (m.find()) {
-			System.out.println("Match found");
-		}
-		else {
-			System.out.println("Match not found");
-		}
+		String Email2 = "rajkawale@gmail.com";		
+		System.out.println(Pattern.compile(regex).matcher(Email2).matches());
 		
-		//To continue the same process by using do while loop
-		System.out.println("Continue...press 1,");
-		ch = scan.nextInt();
-		} 
-		while(ch==1);	
+		String Email3 = "raj_kawale@gmail.com";		
+		System.out.println(Pattern.compile(regex).matcher(Email3).matches());
+		
+		String Email4 = "712@gmail.com";		
+		System.out.println(Pattern.compile(regex).matcher(Email4).matches());
+		
+		String Email5 = "712@gmail.";		
+		System.out.println(Pattern.compile(regex).matcher(Email5).matches());
+		
+		String Email6 = "RAJ712@GMAIL.COM";		
+		System.out.println(Pattern.compile(regex).matcher(Email6).matches());
 	}
 }
