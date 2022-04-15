115a4cee8ec334739c2774ef2d4040308eb66245
Rajkawale7 <Rajkawale712@gmail.com>
2022-04-15 11:55:37 +0530

    Added: Fourth case to check valid Mobile number

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class UserRegistration {
 
 	public static void main(String[] args) {
-		//As a User need to a valid Email ID
+		//As a User need to a follow predefined mobile number format
 		Scanner scan = new Scanner (System.in);
 		int ch = 1;
 		do {
-		System.out.println("Enter Your Email: ");
-		String Email = scan.next();
+		System.out.println("Enter Your Mobile Number: ");
+		String MobNumber = scan.next();
 		
-		String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+		String regex = ("(0/91)?[7-9][0-9]{9}");
 		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(Email);
+		Matcher m = p.matcher(MobNumber);
 		
 		if (m.find()) {
 			System.out.println("Match found");
@@ -26,8 +26,10 @@ public class UserRegistration {
 		System.out.println("Continue...press 1,");
 		ch = scan.nextInt();
 		} 
-		while(ch==1);
-		
+		while(ch==1);	
 	}
-
 }
+
+//(0/91): number starts with (0/91)  
+//[7-9]: starting of the number may contain a digit between 0 to 9  
+//[0-9]: then contains digits 0 to 9
